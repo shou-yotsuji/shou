f0dc3e2e00ea4b0d8123f113ff2fcfeef7fb3464
shou.yotsuji <basketball0420100@gmail.com>
2020-06-20 12:40:28 +0000

    post

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 Rails.application.routes.draw do
   
 
+  get 'toppages/index'
+  get 'posts/create'
+  get 'posts/destroy'
   get 'signup', to: 'users#new'
+  get 'microposts', to: 'microposts#index'
   
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   root to: 'users#index'
   resources :users
+  resources :microposts, only: [:create, :destroy]
+ 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
